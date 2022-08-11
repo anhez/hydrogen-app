e0ea8b58c193793de95d45a910a3b9200cece392
Dat Tran Quoc <dattq@TCrazy.localdomain>
2022-08-11 22:40:32 +0700

    first commit

---
+++ hydrogen.config.js
@@ -0,0 +1,9 @@
+import {defineConfig} from '@shopify/hydrogen/config';
+
+export default defineConfig({
+  shopify: {
+    storeDomain: 'anheasy.myshopify.com',
+    storefrontToken: '2213fd646698203f93ac198d7f4e7564',
+    storefrontApiVersion: '2022-07',
+  },
+});
---
+++ src/routes/index.server.jsx
@@ -0,0 +1,6 @@
+import {Layout} from '~/components/index.server';
+
+export default function Home() {
+  
+  return <Layout>Hello World</Layout>;
+}
